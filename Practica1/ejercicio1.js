ccdfc403bb75af4bce2ae80cd93ef1cc134329be
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2025-05-19 19:42:03 -0600

    Inicio

---
+++ Practica1/ejercicio1.js
@@ -0,0 +1,23 @@
+//------------------------a----------
+var nombre ="Armando";
+var edad = 25;
+
+nombre = "Ana Maria";
+
+var saludo = "Hola " + nombre + ". Tienes " + edad + "años.";
+
+//muestra en consola el contenido del saludo 
+
+//-------------------------b-----------
+function cuadrado(numero){
+    return numero* numero ;
+}
+//console.log();
+//muestra en consola en probando con 3 numeros diferentes
+
+
+//------------------------c------------------
+/*Crea una arrow function llamada saludoPersonalizado que reciba dos parametros: 
+nombre y edad. y retorne una cadena como la siguiente */
+
+//"Hola, me llamo Isay y tengo 37 años ."
