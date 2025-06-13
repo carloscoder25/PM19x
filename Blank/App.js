02c57794a512fd6c7a60d70701732e9631b5a439
carlos030804 <138809228+carlos030804@users.noreply.github.com>
2025-06-12 23:12:28 -0600

    practica 4

--- Blank/App.js
+++ Blank/App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import React,{Children, useState} from 'react';
 
-
-const Texto =(props)=>{
-  const{children}=props
+//componente Propio  Texto 
+const Texto =()=>{
+  const[contenido,setContenido]=useState('Hola mundo')
+  const actualizarTexto=()=>{setContenido('Estado modificado')}
+  const actualizacion=()=>{setContenido('Hola mundo')}
   return(
-    <Text>{children}</Text>
-  )
+    <Text onPress={actualizarTexto}>{ contenido} </Text>
+
+  ) ;   
+  
+}
+
+const Boton =()=>{
+  const [contenido,setContenido]=useState('Soy un boton')
+  const actualizarBoton=()=>{setContenido('ya no soy un boton')}
+  return( 
+ <Button onPress={actualizarBoton} title={contenido}></Button>
+  );
 }
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hola nmmundo REACTNATIVE</Text>
-      <Texto> Hola bb </Texto>
+      <Texto></Texto>
+      <Texto></Texto>
+      <Texto></Texto>
 
       <Button title='Presionar'></Button>
+
+      <Boton title='Soy un boton'></Boton>
+
       <StatusBar style="auto" />
     </View>
   );
